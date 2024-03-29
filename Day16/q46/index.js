a4fbe82ef329410c75bdfe8d46c87d86b5ed52fa
Rida <19103100.rida@gmail.com>
2024-03-29 20:17:35 +0500

    Start 100 days coding challenge

---
+++ Day16/q46/index.js
@@ -0,0 +1,11 @@
+// Enhanced Laptop Object: Construct an object for a laptop including properties make, model, year, and a method describe() that logs a sentence about the laptop.
+// Explain & TIP: Objects can also contain functions (methods) that can perform actions using the object's properties. This introduces method definition within objects.
+var laptop = {
+    make: "Lenovo",
+    model: "Core i5",
+    year: 2022,
+    describe: function () {
+        console.log("This laptop is a ".concat(this.year, " ").concat(this.make, " ").concat(this.model, "."));
+    }
+};
+laptop.describe();
