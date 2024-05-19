3656b9c3e1b9b1ec55fc2bc683be91cddeb0e08d
Rida <19103100.rida@gmail.com>
2024-05-20 01:49:53 +0500

    updated

---
+++ Day67/multiarrar.js
@@ -0,0 +1,5 @@
+"use strict";
+//Type Annotations With Multidimensional Arrays
+let arrayOne = [1, 2, 3, 4, 5];
+let arrayTwo = ["A", "B", "C"];
+let arrayThree = [1, 2, 3, 4, "A", "B", "C"];
---
+++ Day67/type.js
@@ -0,0 +1,8 @@
+"use strict";
+//Type Annotations With Arrays
+// Annotating an array of strings
+let fruits = ["apple", "banana", "cherry"];
+// Using a loop to process each element in the array
+for (let i = 0; i < fruits.length; i++) {
+    console.log(`Fruit: ${fruits[i].toUpperCase()}`);
+}
