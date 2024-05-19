5eb58fcdf500c0da8f27f6d03ed5d05f82bc8d41
Rida <19103100.rida@gmail.com>
2024-05-19 12:39:10 +0500

    updated

---
+++ Day64/index.js
@@ -0,0 +1,23 @@
+/*********************************
+ * TYPE ANNOTATIONS WITH OBJECTS *
+ *********************************/
+var personExOne;
+personExOne = {
+    name: "Alice",
+    age: 30,
+    jobTitle: "Engineer",
+    address: {
+        street: "123 Main St",
+        city: "Wonderland",
+    },
+};
+//   // And there is yet another method
+var personExTwo = {
+    name: "Alice",
+    age: 30,
+    jobTitle: "Engineer",
+    address: {
+        street: "123 Main St",
+        city: "Wonderland",
+    },
+};
