d0a5602ecb0e41941404c99d6f6fd38f2a6673b4
Rida <19103100.rida@gmail.com>
2024-05-21 01:05:17 +0500

    updated

---
+++ Day69/enum.ts
@@ -0,0 +1,13 @@
+enum Days {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+  }
+  
+  const today: Days = Days.Wednesday;
+  
+  console.log(`Today is ${Days[today]}`);
